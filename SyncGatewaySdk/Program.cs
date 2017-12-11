0bbdd85963373cc80eb12df1f9cfded4bef0c92a
Jim Borden <jim.borden@couchbase.com>
2017-12-12 08:14:43 +0900

    Add project files.

---
+++ SyncGatewaySdk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SyncGatewaySdk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
